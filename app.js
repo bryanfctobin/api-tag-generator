acc1b198bdb945c108a2bc5a20e4828ea2dc7311
Bryan Tobin <bryanfctobin@gmail.com>
2020-06-17 08:32:06 -0400

    Creates txt file for each property

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const https = require('https');
 const apiUrl = 'INSERT ADMIRAL ENDPOINT HERE';
 const propId = ['AN ARRAY OF YOUR PROPERTYIDS, PASSED AS STRINGS'];
+const fs = require('fs');
 function getBootstrap(pId) {
     let a = pId;
     let fullUrl = apiUrl + pId + '/bootstrap';
@@ -10,9 +11,13 @@ function getBootstrap(pId) {
             data += chunk;
         });
         resp.on('end', ()=> {
+            let t = "<!-- Admiral Visitor Relationship Management Tag for: -->";
             let x = '<script type="text/javascript">';
             let y = "</script>"
-            let z = x + data + y;
+            let z = t + x + data + y;
+            fs.writeFile(a + ".txt",z,function(err) {
+                if (err) console.log(err);
+            })
             console.log("<!--Admiral Visitor Relationship Management Tag for " + a + "--->");
             console.log(z);
         });
